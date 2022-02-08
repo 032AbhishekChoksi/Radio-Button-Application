3a7e28d2d5a8977bc23162fd6872a343a1093414
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-02-08 19:54:01 +0530

    Radio Button Application @ 2022 #032AbhishekChoksi. All rights reserved.

--- app/src/main/java/com/abhishekchoksi/radiobuttonapplication/MainActivity.java
+++ app/src/main/java/com/abhishekchoksi/radiobuttonapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.abhishekchoksi.radiobuttonapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void radioButtobClickEvent(View view){
+        boolean isChecked = ((RadioButton) view).isChecked();
+        switch (view.getId()){
+            case R.id.rbtMale:
+                if(isChecked){
+                    Toast.makeText(this, "Male is Selected.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.rbtFemale:
+                if(isChecked){
+                    Toast.makeText(this, "Female is Selected.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.rbtOther:
+                if(isChecked){
+                    Toast.makeText(this, "Other is Selected.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
